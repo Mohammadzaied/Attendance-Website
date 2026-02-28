fed2d6c6b7d0eaab56f33697aa5706c3d02398ab
Mohammad Zaied <mohammad.zaied.8@gmail.com>
2026-03-01 00:44:50 +0200

    edit url

--- lib/api.ts
+++ lib/api.ts
@@ -8,8 +8,8 @@ import axios, {
 // Import offline banner control
 
 // API Configuration
-export const API_BASE_URL = "https://localhost:7094";
-// export const API_BASE_URL = "https://attendance-rwtc.runasp.net";
+// export const API_BASE_URL = "https://localhost:7094";
+export const API_BASE_URL = "https://attendance-rwtc.runasp.net";
 
 // Public endpoints that don't require authentication cookies
 const PUBLIC_ENDPOINTS = ["/Users/SignIn"];
