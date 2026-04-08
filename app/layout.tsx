9fd8527d590dfefccbd86d5c8512337b5f200c07
Mohammad Zaied <mohammad.zaied.8@gmail.com>
2026-04-08 09:49:03 +0300

    edit logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,24 +14,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "RWTC",
+  description: "RWTC Attendance Dashboard",
+  generator: "RWTC",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: "/rwtc_hdr.gif",
+
     apple: "/apple-icon.png",
   },
 };
